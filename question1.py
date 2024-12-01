46eac6063cf751db29c8a2b091ba5ea16dbf3c5d
tamerlan <tamerlan@chalmers.se>
2024-12-01 15:32:07 +0100

    Question 1 part 1

--- question1.py
+++ question1.py
@@ -1,1 +1,32 @@
-YOYO
+
+def readFile():
+    with open ("question1.txt","r") as file:
+      data = file.read()
+    return data
+    
+    
+data = readFile()
+array_data = data.split("\n")
+#print(array_data)
+
+new_array = []
+for str in array_data:
+    new_array.append(str.split())
+    
+
+arr1 = []
+arr2 = []
+for arr in new_array:
+    strnmr = arr[0]
+    arr1.append(int(strnmr))
+    strnmr = arr[1]
+    arr2.append(int(strnmr))
+        
+arr1.sort()
+arr2.sort()
+
+result = 0
+for i in range (len(arr1)):
+    result += abs(arr1[i] - arr2[i])
+
+print(result)
